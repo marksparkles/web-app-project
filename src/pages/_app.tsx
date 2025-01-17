9fa307d75cd3ea5ef74c414c7f6f78b85d4d1f21
Mark Lindsay <marklindsay@Marks-Mac-mini.local>
2025-01-17 19:37:50 +1000

    more

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,9 @@
-import '../styles/globals.css'
+import '@/styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
 
