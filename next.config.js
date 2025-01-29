9354cc95db90d11f7cda443693f1e0beb38b7deb
Mark Lindsay <marklindsay@Marks-Mac-mini.local>
2025-01-29 23:00:59 +1000

    ditched everything

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Remove any custom configuration for now
-}
-
-module.exports = nextConfig
-
--- public/jobs/[id]/work-update/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function WorkUpdateLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return children
-  }
-  
-  
